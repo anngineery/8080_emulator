4d0c5a1793c4efcef3fcd7fa35dd84aa70bd6f15
Ann Yun <by.ann.yun@gmail.com>
2021-03-23 08:53:07 -0400

    Added in Increment and Decrement instructions

--- emulator.c
+++ emulator.c
@@ -7,7 +7,7 @@ typedef struct ConditionCodes {
     uint8_t    s:1;
     uint8_t    p:1;
     uint8_t    cy:1;
-    uint8_t    ac:1;
+    uint8_t    ac:1;    // Axiliary Carry is ignored, since I cannot test it
     uint8_t    pad:3;
 } ConditionCodes;
 
@@ -43,65 +43,230 @@ void Emulate8080Op(State8080* state) {
         case 0x00: UnimplementedInstruction(state); break;
         case 0x01: UnimplementedInstruction(state); break;
         case 0x02: UnimplementedInstruction(state); break;
-        case 0x03: UnimplementedInstruction(state); break;
-        case 0x04: UnimplementedInstruction(state); break;
-        case 0x05: UnimplementedInstruction(state); break;
+        case 0x03:  // INX B
+            break;
+
+        case 0x04:  // INR B
+        {
+            uint8_t answer = state->b + 1;  // Store in 8 bits, bc we don't care about carry flag
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->b = answer;
+
+            break;
+        }
+
+        case 0x05:  // DCR B
+        {
+            uint8_t answer = state->b - 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->b = answer;
+
+            break;
+        }
+
+
         case 0x06: UnimplementedInstruction(state); break;
         case 0x07: UnimplementedInstruction(state); break;
         case 0x08: UnimplementedInstruction(state); break;
         case 0x09: UnimplementedInstruction(state); break;
         case 0x0a: UnimplementedInstruction(state); break;
         case 0x0b: UnimplementedInstruction(state); break;
-        case 0x0c: UnimplementedInstruction(state); break;
-        case 0x0d: UnimplementedInstruction(state); break;
+        case 0x0c:  // INR C
+        {
+            uint8_t answer = state->c + 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->c = answer;
+
+            break;
+        }
+
+        case 0x0d:  // DCR C
+        {
+            uint8_t answer = state->c - 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->c = answer;
+
+            break;
+        }
+
+
         case 0x0e: UnimplementedInstruction(state); break;
         case 0x0f: UnimplementedInstruction(state); break;
         case 0x10: UnimplementedInstruction(state); break;
         case 0x11: UnimplementedInstruction(state); break;
         case 0x12: UnimplementedInstruction(state); break;
         case 0x13: UnimplementedInstruction(state); break;
-        case 0x14: UnimplementedInstruction(state); break;
-        case 0x15: UnimplementedInstruction(state); break;
+        case 0x14:  // INR D
+        {
+            uint8_t answer = state->d + 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->d = answer;
+
+            break;
+        }
+
+        case 0x15:  // DCR D
+        {
+            uint8_t answer = state->d - 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->d = answer;
+
+            break;
+        }
+
         case 0x16: UnimplementedInstruction(state); break;
         case 0x17: UnimplementedInstruction(state); break;
         case 0x18: UnimplementedInstruction(state); break;
         case 0x19: UnimplementedInstruction(state); break;
         case 0x1a: UnimplementedInstruction(state); break;
         case 0x1b: UnimplementedInstruction(state); break;
-        case 0x1c: UnimplementedInstruction(state); break;
-        case 0x1d: UnimplementedInstruction(state); break;
+        case 0x1c:  // INR E
+        {
+            uint8_t answer = state->e + 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->e = answer;
+
+            break;
+        }
+
+        case 0x1d:  // DCR E
+        {
+            uint8_t answer = state->e - 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->e = answer;
+
+            break;
+        }
+
         case 0x1e: UnimplementedInstruction(state); break;
         case 0x1f: UnimplementedInstruction(state); break;
         case 0x20: UnimplementedInstruction(state); break;
         case 0x21: UnimplementedInstruction(state); break;
         case 0x22: UnimplementedInstruction(state); break;
         case 0x23: UnimplementedInstruction(state); break;
-        case 0x24: UnimplementedInstruction(state); break;
-        case 0x25: UnimplementedInstruction(state); break;
+        case 0x24:  // INR H
+        {
+            uint8_t answer = state->h + 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->h = answer;
+
+            break;
+        }
+
+        case 0x25:  // DCR H
+        {
+            uint8_t answer = state->h - 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->h = answer;
+
+            break;
+        }
+
         case 0x26: UnimplementedInstruction(state); break;
         case 0x27: UnimplementedInstruction(state); break;
         case 0x28: UnimplementedInstruction(state); break;
         case 0x29: UnimplementedInstruction(state); break;
         case 0x2a: UnimplementedInstruction(state); break;
         case 0x2b: UnimplementedInstruction(state); break;
-        case 0x2c: UnimplementedInstruction(state); break;
-        case 0x2d: UnimplementedInstruction(state); break;
+        case 0x2c:  // INR L
+        {
+            uint8_t answer = state->l + 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->l = answer;
+
+            break;
+        }
+
+        case 0x2d:  // DCR L
+        {
+            uint8_t answer = state->l - 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->l = answer;
+
+            break;
+        }
+
         case 0x2e: UnimplementedInstruction(state); break;
         case 0x2f: UnimplementedInstruction(state); break;
         case 0x30: UnimplementedInstruction(state); break;
         case 0x31: UnimplementedInstruction(state); break;
         case 0x32: UnimplementedInstruction(state); break;
         case 0x33: UnimplementedInstruction(state); break;
-        case 0x34: UnimplementedInstruction(state); break;
-        case 0x35: UnimplementedInstruction(state); break;
+        case 0x34:  // INR M
+        {
+            uint16_t offset = (state->h<<8) | (state->l);
+            uint8_t answer = state->memory[offset] + 1;
+            state->cc.z = ((answer & 0xff) == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->memory[offset] = answer;
+
+            break;
+        }
+
+        case 0x35:  // DCR M
+        {
+            uint16_t offset = (state->h<<8) | (state->l);
+            uint8_t answer = state->memory[offset] - 1;
+            state->cc.z = ((answer & 0xff) == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->memory[offset] = answer;
+
+            break;
+        }
+
         case 0x36: UnimplementedInstruction(state); break;
         case 0x37: UnimplementedInstruction(state); break;
         case 0x38: UnimplementedInstruction(state); break;
         case 0x39: UnimplementedInstruction(state); break;
         case 0x3a: UnimplementedInstruction(state); break;
         case 0x3b: UnimplementedInstruction(state); break;
-        case 0x3c: UnimplementedInstruction(state); break;
-        case 0x3d: UnimplementedInstruction(state); break;
+        case 0x3c:  // INR A
+        {
+            uint8_t answer = state->a + 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->a = answer;
+
+            break;
+        }
+
+        case 0x3d:  // DCR A
+        {
+            uint8_t answer = state->a - 1;
+            state->cc.z = (answer == 0);
+            state->cc.s = ((answer & 0x80) != 0);
+            state->cc.p = Parity(answer);
+            state->a = answer;
+
+            break;
+        }
         case 0x3e: UnimplementedInstruction(state); break;
         case 0x3f: UnimplementedInstruction(state); break;
         case 0x40: UnimplementedInstruction(state); break;
